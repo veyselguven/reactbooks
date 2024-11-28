cde587007f28e2d8cbe438e23af5e93e486c4704
veyselguven <vgbasbaydar@gmail.com>
2024-11-28 08:35:31 -0800

    tested

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -5,6 +5,7 @@ function BookCreate({ onCreate }) {
   // console.log(onCreate);
 
   const [title, setTitle] = useState("");
+
   const handleChange = (event) => {
     setTitle(event.target.value);
   };
@@ -19,8 +20,13 @@ function BookCreate({ onCreate }) {
     <div className="book-create">
       <h3>Add a Book</h3>
       <form onSubmit={handleSubmit}>
-        <label>Title</label>
-        <input className="input" value={title} onChange={handleChange} />
+        <label for="titlee">Title</label>
+        <input
+          id="titlee"
+          className="input"
+          value={title}
+          onChange={handleChange}
+        />
         <button className="button">Create!</button>
       </form>
     </div>
